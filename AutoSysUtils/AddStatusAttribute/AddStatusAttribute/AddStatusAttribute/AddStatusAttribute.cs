cba2d0e21df07966602194e74dfa5e7d38a87bd2
Deekshit Addepalli <kesavadeekshit@outlook.com>
2018-09-24 11:29:58 -0500

    Code to add status attribute to jil file

---
+++ AutoSysUtils/AddStatusAttribute/AddStatusAttribute/AddStatusAttribute/AddStatusAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.IO;
+
+namespace AddStatusAttribute
+{
+    class AddStatusAttribute
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        StreamReader jilFileReader;
+        StreamWriter statusJilWriter;
+
+        static void Main(string[] args)
+        {
+            AddStatusAttribute asj = new AddStatusAttribute();
+            Console.WriteLine("Enter the full path to the jil file: ");
+            string jilFilePath = Console.ReadLine();
+            Console.WriteLine("The file is at " + jilFilePath);
+            if (File.Exists(jilFilePath))
+            {
+                asj.jilFileReader = new StreamReader(jilFilePath);
+                string fileName = Path.GetFileNameWithoutExtension(jilFilePath);
+                string folderName = Path.GetDirectoryName(jilFilePath);
+                string copyFileName = folderName + "\\"+fileName + "_copy.txt";
+                string statusFileName = folderName + "\\"+fileName + "_status.txt";
+                asj.statusJilWriter = new StreamWriter(statusFileName);
+                File.Copy(jilFilePath, copyFileName,true);
+                string currentJilLine = null;
+                while((currentJilLine=asj.jilFileReader.ReadLine())!=null)
+                {
+                    if(currentJilLine.Contains("insert_job:"))
+                    {
+
+                        asj.statusJilWriter.WriteLine(currentJilLine);
+                        asj.statusJilWriter.WriteLine("status: ON_ICE");
+                        continue;
+                    }
+                    asj.statusJilWriter.WriteLine(currentJilLine);
+                    
+                }
+            }
+            asj.statusJilWriter.Close();
+            asj.jilFileReader.Close();
+        }
+    }
+}
---
+++ AutoSysUtils/AddStatusAttribute/AddStatusAttribute/AddStatusAttribute/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AddStatusAttribute")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AddStatusAttribute")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: log4net.Config.XmlConfigurator(Watch = true)] // This line is required for logging with log4net to work.
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2f8079a-0d96-4503-8a72-e671757e25d5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
